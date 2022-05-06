b4f8af7f00c6fce6ecfc24e615fd14e3a66e9b14
DragonGamesStudios <margos.michal@gmail.com>
2022-05-06 17:09:17 +0200

    Update script update

--- tools/update.sh
+++ tools/update.sh
@@ -1,10 +1,10 @@
-git checkout future
+git checkout experimental
 git pull
 git checkout master
 git pull
-git merge future
+git merge experimental
 rm Release/*.zip
 python3 tools/tracker.py
 git commit -m "New release"
 git push origin master
-git checkout future
+git checkout experimental
